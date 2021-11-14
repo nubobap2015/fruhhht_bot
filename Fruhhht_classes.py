cadebe966626eff4ae88a290b02ce6e7ef37cb9c
Sergey Kopeykin <nubobap2015@yandex.ru>
2021-11-14 20:21:17 +0300

    0003

--- Fruhhht_classes.py
+++ Fruhhht_classes.py
@@ -11,7 +11,8 @@ class FruhhhtBot():
     def __init__(self, bot, chat_id):
         self.bot = bot
         self.chat_id = chat_id
-        self.bot_timer = threading.Timer(BOT_INTERVAL, self.bot_activity)
+        self.chat = self.bot.get_chat(self.chat_id)
+        self.bot_timer = threading.Timer(BOT_INTERVAL, self._bot_activity)
         if DEBUG_MODE:
             print(self.bot, self.chat_id, self.bot_timer)
 
@@ -25,38 +26,55 @@ class FruhhhtBot():
     def get_message(self, message):
         pass
 
-    def bot_activity(self):
+    def send_message(self, text_message):
+        self.bot.send_message(self.chat_id, text_message)
+
+    def set_chat2DB(self):
+        res = self._select_from_db(f'select * from chats where id_chat = {self.chat_id}')
+        #if len(res)>0:
+
+
+
+    def _bot_activity(self):
         self.bot.send_message(self.chat_id, f"Активность")
         self.bot_timer = threading.Timer(BOT_INTERVAL, self.bot_activity)
         self.bot_timer.start()
 
-
-    def select_from_db(self, sql_text):
+    def _select_from_db(self, sql_text):
         with SQL() as sql:
             return sql.fetchall(sql_text)
 
-    def insert_into_db(self, sql_text):
+    def _insert_into_db(self, sql_text):
         with SQL() as sql:
             return sql.insert(sql_text)
 
+    def _update_db(self, sql_text):
+        with SQL() as sql:
+            return sql.update(sql_text)
+
 
 class SQL():
+
     def __enter__(self):
         self.conn = sqlite3.connect(DB_NAME)
         self.cur = self.conn.cursor()
         return self
+
     def __exit__(self, type, value, traceback):
         # Exception handling here
         self.cur.close()
         self.conn.close()
+
     def fetchall(self, sql):
         return self.cur.execute(sql).fetchall()
+
     def insert(self, sql):
         self.cur.execute(sql)
         ins_id = self.cur.lastrowid
         self.conn.commit()
         return ins_id
 
-
-
-
+    def update(self, sql):
+        self.cur.execute(sql)
+        self.conn.commit()
+        return True
--- fruhhht_me.py
+++ fruhhht_me.py
@@ -5,23 +5,28 @@ from Fruhhht_classes import FruhhhtBot
 bot = telebot.TeleBot(secrets.bot_token)
 bot_list = {}
 
+
 @bot.message_handler(commands=['status_server'])
 def welcome(message):
     print(f'STATUS_SERVER: {message.from_user.username}: {message.text}')
     print(bot_list)
 
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
     print(f'START: {message.from_user.username}: {message.text}')
-    print(message)
-    id_chat = message.chat.id
-    id_chat_str = str(id_chat)
-    my_bot = bot_list.get(id_chat_str)
-    if my_bot:
-        pass
+    if message.get('chat').get('type') == 'group':
+        # print(message)
+        id_chat = message.chat.id
+        id_chat_str = str(id_chat)
+        my_bot = bot_list.get(id_chat_str)
+        if my_bot:
+            pass
+        else:
+            bot_list[id_chat_str] = FruhhhtBot(bot, id_chat)
+            bot_list[id_chat_str].start()
     else:
-        bot_list[id_chat_str] = FruhhhtBot(bot, id_chat)
-        bot_list[id_chat_str].start()
+        print('Это не группа (((( ')
 
 
 @bot.message_handler(commands=['stop'])
